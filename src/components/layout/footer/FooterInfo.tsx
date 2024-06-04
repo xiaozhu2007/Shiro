84c9655282da7b8d620ddfc261cd4e432c1c2d0e
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2024-06-04 14:26:34 +0800

    页脚信息修改

--- src/components/layout/footer/FooterInfo.tsx
+++ src/components/layout/footer/FooterInfo.tsx
@@ -121,7 +121,7 @@ const PoweredBy: Component = ({ className }) => {
         mobileAsSheet
         type="tooltip"
         triggerElement={
-          <StyledLink href="https://github.com/innei/Shiro" target="_blank">
+          <StyledLink href="https://github.com/xiaozhu2007/Shiro" target="_blank">
             Shiro
           </StyledLink>
         }
@@ -134,12 +134,7 @@ const PoweredBy: Component = ({ className }) => {
         >
           Shiro
         </StyledLink>{' '}
-        的开源版本。
-        {process.env.COMMIT_HASH && process.env.COMMIT_URL && (
-          <MLink popper={false} href={process.env.COMMIT_URL}>
-            版本哈希：{process.env.COMMIT_HASH.slice(0, 8)}
-          </MLink>
-        )}
+        的开源二改版本。
       </FloatPopover>
       .
     </span>
@@ -194,6 +189,10 @@ const FooterBottom = async () => {
           <a href="/sitemap.xml" target="_blank" rel="noreferrer">
             站点地图
           </a>
+          <Divider />
+          <a href="https://www.qiudcdn.cn/" target="_blank" rel="noreferrer">
+            本站由 QiuDN 提供加速服务
+          </a>
           <Divider className="inline" />
 
           <SubscribeTextButton>
@@ -201,7 +200,7 @@ const FooterBottom = async () => {
           </SubscribeTextButton>
         </span>
         <span className="mt-3 block md:mt-0 md:inline">
-          Stay hungry. Stay foolish.
+          保持饥饿，保持愚蠢
         </span>
       </p>
       <div>
