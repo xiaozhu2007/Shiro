9e2d100c01c12bd4695a2092299fdd835047d30f
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2024-06-05 12:57:56 +0800

    Update Activity.tsx

--- src/components/layout/header/internal/Activity.tsx
+++ src/components/layout/header/internal/Activity.tsx
@@ -32,7 +32,7 @@ const ActivityIconContext = createContext<{
   appDescription: any
 }>(null!)
 
-const CND_DOMAIN = 'https://fastly.jsdelivr.net/gh/Innei/reporter-assets@main'
+const CND_DOMAIN = 'https://fastly.jsdelivr.net/gh/xiaozhu2007/r-assets@main'
 const fetchJsonData = () => {
   return Promise.all([
     fetch(`${CND_DOMAIN}/app-icon.json`).then((res) => res.json() as object),
